955e06c040361d04048809d49b05e3d5d4d09dec
madhuradhawangale00-sys <madhuradhawangale00@gmail.com>
2026-09-17 00:42:50 +0530

    fix app.js and ejs file

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const path = require("path");
 const http = require("http");
-
 const socketio = require("socket.io");
 
-const server = http.createServer(app);
+const app = express();
 
+const server = http.createServer(app);
 const io = socketio(server);
 
 app.set("view engine", "ejs");
-app.set(express.static(path.join(__dirname, "public")));
 
-app.get("/", function (req,res){
-    res.send("hey");
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", function (socket) {
+    console.log("connected");
+
+    socket.on("disconnect", function () {
+        console.log("disconnected");
+    });
 });
 
+app.get("/", function (req, res) {
+    res.render("index");
+});
 
-server.listen(3000);
+server.listen(3000, function () {
+    console.log("Server running on http://localhost:3000");
+});
